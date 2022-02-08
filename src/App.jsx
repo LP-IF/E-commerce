2b962c3b8040b7a5697d90aee8e3b4c7439e2a65
Gabriel Faria <gabriel.selu@gmail.com>
2022-02-08 14:46:59 -0300

    adiciona componente responsavel por formularios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState } from 'react';
+
+import Campos from './components/form/Campos';
+
+export default (props) => (
+  <div className="App">
+    <Campos>
+
+    </Campos>
+  </div>
+)
+
