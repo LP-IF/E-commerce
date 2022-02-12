a2f506546c4209912ec1f220e288246c53c23f38
g_coelho <gabriel_coelho_s@yahoo.com.br>
2022-02-11 21:23:42 -0300

    Inicio da implementação da tela de Categoria(RF05)

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import React, { useState } from 'react';
 import CadastroCliente from './components/form/CadastroCliente'
 import Login from './components/form/Login';
 import CadastroProd from './components/form/CadastroProd';
+import Categoria from './components/form/Categoria';
+import Botao from './components/form/Botao';
 
 export default (props) => (
   <div className="App">
 
     {/* <Login/> */}
     {/* <CadastroCliente/>  */}
-    <CadastroProd/> 
+    {/* <CadastroProd/> */}
+    <Categoria/>
+     
 
   </div>
 )
