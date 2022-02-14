bb68920935c8cb905a02a7b95d7fe04b24526d55
Gabriel Faria <gabriel.selu@gmail.com>
2022-02-14 20:47:24 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from './components/form/Login';
 import CadastroProd from './components/form/CadastroProd';
 import Categoria from './components/form/Categoria';
 import CompraFalta from './components/form/CompraFalta';
-import ProdutoRecebido from './components/form/ProdutosRecebidos';
+import ProdutosRecebidos from './components/form/ProdutosRecebidos';
 import ProblemaVenda from './components/form/ProblemaVenda';
 import CadastrarCartao from './components/form/CadastrarCartao';
 import CadastroAdmin from './components/form/CadastroAdmin';
@@ -17,12 +17,12 @@ import CadastroPJ from './components/form/CadastroPJ';
 export default (props) => (
   <div className="App">
 
-     <Login/> 
+     {/* <Login/>  */}
     {/* <CadastroCliente/> */}
     {/* <CadastroProd/> */}
     {/* <Categoria/>  */}
     {/* <CompraFalta/> */}
-     {/* <ProdutosRecebidos/> */}
+     <ProdutosRecebidos/>
     {/* <ProblemaVenda/> */}
     {/* <CadastrarCartao/> */}
      {/* <CadastroAdmin/> */}
